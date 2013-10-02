3f0ba59023c9f021e7c7f3e167a60f85f1a5b76d
tavadi <Benitho@Benjamin-Thorstensens-MacBook-Pro.local>
2013-10-02 16:57:39 +0200

    Client: listen eingebaut(wartet auf OK vom server) // Server: Send funktion fertig!

--- myclient.cpp
+++ myclient.cpp
@@ -28,17 +28,21 @@
 #include <iostream>
 using namespace std;
 #define BUF 1024
-#define PORT 6543
+//#define PORT 6543
 
 int main (int argc, char **argv) {
   int create_socket;
   char buffer[BUF];
   struct sockaddr_in address;
   int size;
+  int port;
 
-  if( argc < 2 ){
+  if( argc < 3 ){
      printf("Usage: %s ServerAdresse\n", argv[0]);
      exit(EXIT_FAILURE);
+  }else
+  {
+    port = atoi(argv[2]);
   }
 
   if ((create_socket = socket (AF_INET, SOCK_STREAM, 0)) == -1)
@@ -49,15 +53,14 @@ int main (int argc, char **argv) {
   
   memset(&address,0,sizeof(address));
   address.sin_family = AF_INET;
-  address.sin_port = htons (PORT);    
+  address.sin_port = htons (port);    
   inet_aton (argv[1], &address.sin_addr);   // ist schon in networkbyte order
 
   if (connect ( create_socket, (struct sockaddr *) &address, sizeof (address)) == 0) //kein bind sondern connect
   {
       printf ("Connection with server (%s) established\n", inet_ntoa (address.sin_addr));
       size=recv(create_socket,buffer,BUF-1, 0);  //willkommensnachricht wird eingelesen
-      if (size>0)
-      {
+      if (size>0){
         buffer[size]= '\0';
         printf("%s",buffer);
       }
@@ -69,9 +72,16 @@ int main (int argc, char **argv) {
   } 
   printf ("Send message: ");
   do {
+
      fgets (buffer, BUF, stdin);  //eingabe wird eingelesen
 
      send(create_socket, buffer, strlen (buffer), 0); //gesendet
+
+      size=recv(create_socket,buffer,BUF-1, 0);  //status wird gesendet OK oder ERR
+      if (size>0){
+        buffer[size]= '\0';
+        printf("%s",buffer);
+      }
   } 
   while (strcmp (buffer, "quit\n") != 0);
   close (create_socket);
--- myserver.cpp
+++ myserver.cpp
@@ -38,38 +38,48 @@ Muss in Sende funktion gepackt werden
 #include <iostream>
 #include <time.h>  
 #include <sstream>
+#include <errno.h>
 using namespace std;
 #define BUF 1024
-#define PORT 6543
-
-// ##################################################################################################################################################################
-// Prototypen
-int send(char * buffer, int status,string & mailname);
+//define PORT 6543
+#define OK "OK\n"
+#define ERR "ERR\n"
+//*************************************************************************
+//******************************Prototypen*********************************
+// 
+int SaveMail(char * buffer, int status,string & mailname, string & Mail);
 int list();
-
-// ##################################################################################################################################################################
+//*************************************************************************
+//*************************************************************************
 
   
-int main (void) {
+int main (int argc, char **argv) {
   int create_socket, new_socket;
   socklen_t addrlen;
   char buffer[BUF];
   int size;
   struct sockaddr_in address, cliaddress; // speichert ip von client und server
   int status = 0;  //dient als info für den aktuellen zustand
+  int laststatus = 0;
   int stat; //fürs ordner erstellen
   FILE *fp;
   string mailname;
-  
+  string Mail;
+  int port;
 
-
-
+ if( argc < 2 ){
+     printf("Usage: %s ServerAdresse\n", argv[0]);
+     exit(EXIT_FAILURE);
+  }else
+  {
+    port = atoi(argv[1]);
+  }
   create_socket = socket (AF_INET, SOCK_STREAM, 0); //Socket descriptor angelegt // af_inet = ipv4 // SOCK_Stream = TCP ... SOCK DATAGRAM = UPD
 
   memset(&address,0,sizeof(address)); // Wird mit 0 initalisiert damit kein müll drinnen steht
   address.sin_family = AF_INET;   //ipv4
   address.sin_addr.s_addr = htonl(INADDR_ANY);  // htonl(INADDR_ANY)
-  address.sin_port = htons (PORT);  //Port wird angegeben (6543), Byte order wichtig
+  address.sin_port = htons (port);  //Port wird angegeben (6543), Byte order wichtig
 
   if (bind ( create_socket, (struct sockaddr *) &address, sizeof (address)) != 0) { //Socket descriptor wird an den port gebunden
      perror("bind error");
@@ -89,31 +99,41 @@ int main (void) {
         send(new_socket, buffer, strlen(buffer),0);
      }
      do {
-        size = recv (new_socket, buffer, BUF-1, 0); //holt sich die nachricht
+        size = recv (new_socket, buffer, BUF-1, 0); //holt sich die nachrich
+
+
+       ///**************Arbeitsgebiet**************************
+
+        if (status == -1) // FEHLER
+        {
+          status = laststatus;  //Status wird zurückgesetzt
+        }
+        else{             //Kein Fehler
+          laststatus = status;  //Status wird gespeichert
+        }
         if( size > 0)
         {
-           buffer[size] = '\0';   //ende angeben sonst möglicher overflow
-
-           ///****************************************
-           cout << "status: {"<< status << "}-------------" <<endl;
-           if (strcmp(buffer, "SEND\n") == 0  || status == 1 ||status == 2 ||status == 3||status == 4) {
-           if (strcmp(buffer, "SEND\n") == 0){status =0;} 
-          status=(send(buffer, status, mailname));}
-           else if (strcmp(buffer, "LIST\n") == 0) {status = 4;
-            list();}
+           buffer[size] = '\0';   //Ende angeben sonst möglicher Overflow
+           //cout << "status: {"<< status << "}-------------" << "laststatus : {" << laststatus << "}" <<endl;
+           if (strcmp(buffer, "SEND\n") == 0  || status == 1 ||status == 2 ||status == 3||status == 4 || status == 5){
+             if((status=(SaveMail(buffer, status, mailname, Mail))) == -1 ){
+                send(new_socket, ERR, strlen(ERR),0);
+             }else{
+              send(new_socket, OK, strlen(OK),0);
+             }
+           }
+           else if (strcmp(buffer, "LIST\n") == 0) {status = 4;list();}
            else if (strcmp(buffer, "READ\n") == 0) {status = 3;}
            else if (strcmp(buffer, "DEL\n") == 0)  {status = 4;}
            printf ("Message received: %s\n", buffer);   //schreibt bis zum \0
         }
 
         //**********************************************
-        else if (size == 0)
-        {
+        else if (size == 0){
            printf("Client closed remote socket\n");
            break;
         }
-        else
-        {
+        else{
            perror("recv error");
            return EXIT_FAILURE;
         }
@@ -134,51 +154,91 @@ const string currentDateTime() {
     return buf;
 }
 
-int send(char * buffer,int status, string & mailname){
+
+
+//*************************************************************************
+//*************************************************************************
+//*************************************************************************
+
+
+int SaveMail(char * buffer,int status, string & mailname, string & Mail){
   FILE *fp;
   int stat;//fürs ordner erstellen
 
-  
-  if(status == 4)
-  {
-    if (strcmp(buffer, ".\n") != 0) {
-    fp = fopen(mailname.c_str(), "ab+");
-    fputs(buffer,fp); 
-    fclose(fp);
 
-    }else if(strcmp(buffer, ".\n") == 0){status = 0;chdir("..");}
-   
+  if(status == 5)
+  { 
+    if (strcmp(buffer, ".\n") != 0) {
+    Mail.append(buffer);
+    }else if(strcmp(buffer, ".\n") == 0){
+      fp = fopen(mailname.c_str(), "ab+");
+      if (fp == NULL){
+        printf ("Error opening file unexist.ent: %s\n",strerror(errno));
+       return -1;
+      }else{
+        fputs(Mail.c_str(),fp); 
+        fclose(fp);
+        status = 0;
+        chdir("..");
+        Mail.erase();
+      }
+    }
   }
-  else if(status == 3) // <Nachricht, beliebige Anzahl an Zeilen\n>
-  {
-    fp = fopen(mailname.c_str(), "ab+");
-    fputs(buffer,fp); 
-    fclose(fp);
-    
-    status = 4;    
+  else if(status == 4){ // <Nachricht, beliebige Anzahl an Zeilen\n>
+    Mail.append(buffer);
+    status = 5;    
   }
-  else if(status == 2) //<Betreff max. 80 Zeichen>\n 
-  {
-    mailname = currentDateTime();
-    mailname.append("_");
-    mailname.append(buffer);
-    mailname.erase(remove(mailname.begin(), mailname.end(), '\n'), mailname.end());
-    mailname.append(".txt");
-    fp = fopen(mailname.c_str(), "ab+");
-    status = 3;              
-  }
-  else if(status == 1) // <Empfänger max. 8 Zeichen>\n
-  {
-    stat = mkdir(buffer, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-    chdir(buffer); // in den ordner rein für messages.txt
-    status = 2;
-  }
-  else if(status == 0)
-  {
+  else if(status == 3){ //<Betreff max. 80 Zeichen>\n 
+    if((strlen(buffer)) <= 81){
+      mailname = currentDateTime();
+      mailname.append("_");
+      mailname.append(buffer);
+      mailname.erase(remove(mailname.begin(), mailname.end(), '\n'), mailname.end());
+      mailname.append(".txt");
+      Mail.append(buffer);
+      fp = fopen(mailname.c_str(), "ab+");  //File erstellen
+      if (fp == NULL){
+        printf ("Error opening file unexist.ent: %s\n",strerror(errno));
+        return -1;
+      }
+      else{
+        fclose(fp);
+        status = 4; 
+      }
+      
+    }else{
+      return -1;
+    }             
+  }else if(status == 2) // <Empfänger max. 8 Zeichen>\n
+  { 
+    if((strlen(buffer)) <= 9){
+      stat = mkdir(buffer, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+      if (chdir(buffer) != 0){  // in den rdner rein um auf Mail zuzugreifen
+        perror("chdir() to /tmp failed");
+      } 
+      Mail.append(buffer);
+      status = 3;
+    }else{
+      return -1;
+    }
+  }else if(status == 1){ //<Sender max. 8 Zeichen>\n
+    if((strlen(buffer)) <= 9){
+      Mail.append(buffer);
+      status = 2;
+    }else{
+      return -1;
+    }
+  }else if(status == 0){  //SEND wird initalisiert 
     status = 1;
   }
   return status;
 }
+
+
+
+//*************************************************************************
+//*************************************************************************
+//*************************************************************************
 
 
 int list (){
